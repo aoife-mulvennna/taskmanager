2353596286c03b25ee7b9cddb23dad5cc2738f1e
aoife <amulvenna10@qub.ac.uk>
2025-05-24 21:05:46 +0100

    adds crud api calls

---
+++ backend/src/main/java/com/example/backend/controller/TaskController.java
@@ -0,0 +1,55 @@
+package com.example.backend.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.backend.model.Task;
+import com.example.backend.repository.TaskRepository;
+
+@RestController
+@RequestMapping("/api/tasks")
+@CrossOrigin(origins = "http://localhost:5173")
+public class TaskController {
+    
+    private final TaskRepository repository;
+
+    public TaskController(TaskRepository repository){
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks(){
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task){
+        return repository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task updatedTask){
+        return repository.findById(id)
+        .map(task -> {
+            task.setTitle(updatedTask.getTitle());
+            task.setDescription(updatedTask.getDescription());
+            task.setCompleted(updatedTask.isCompleted());
+            return repository.save(task);
+        })
+        .orElseThrow(() -> new RuntimeException("Task not found"));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+}
